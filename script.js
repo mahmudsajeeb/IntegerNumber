0956a8d2f3c5f8098c01014c2d1e860d3c91884e
mahmudsajib <mahmudsajib082@gmail.com>
2022-03-05 01:18:30 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// const num = Math.ceil(Math.random()*10)
+
+// const enterNumber =prompt("Enter Integer Number")
+
+// if(enterNumber ==num){
+//   console("Matched")
+// }else{
+//   console.log("Not Match Number")
+// }
+
+const num = Math.ceil(Math.random() * 10)
+
+const enterNumber = prompt("Enter your Integer Number")
+
+if(enterNumber == num){
+  console.log("Number Match")
+}else{
+  console.log("Number Not Match")
+}
